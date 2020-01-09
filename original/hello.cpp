5cc86d973aa49e4cdd5328e77ab23ddb4a5f1553
Paul Barsic <pbarsic@gmail.com>
2020-01-09 11:48:48 -0800

    trivial change

--- original/hello.cpp
+++ original/hello.cpp
@@ -10,6 +10,7 @@ int main(void) {
   cout << "My name is Inigo Montoya.\n";
   cout << "You killed my father.\n";
   cout << "Prepare to die." << endl;
+  cout << "You have an over-developed sense of vengence.\n";
 
   return 0;
 }
